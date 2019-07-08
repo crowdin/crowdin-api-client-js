c3d28c6bae23e7dae878b38061acd5eca63bf7cf
Yevheniy Oliynyk <evgen41293@gmail.com>
2019-07-08 12:50:44 +0300

    added docs

--- src/branches/index.ts
+++ src/branches/index.ts
@@ -7,6 +7,12 @@ export namespace Branches {
 
     export class Api extends CrowdinApi {
 
+        /**
+         * @param projectId project identifier
+         * @param name filter branch by name
+         * @param limit maximum number of items to retrieve (default 25)
+         * @param offset starting offset in the collection (default 0)
+         */
         listProjectBranches(projectId: number, name?: string, limit?: number, offset?: number): Promise<ResponseList<Model.Branch>> {
             let url = `${this.url}/projects/${projectId}/branches?account-key=${this.accountKey}&login=${this.login}`;
             url = this.addQueryParam(url, 'name', name);
@@ -15,22 +21,39 @@ export namespace Branches {
             return axios.get(url);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param request request body
+         */
         createBranch(projectId: number, request: Model.CreateBranchRequest): Promise<ResponseObject<Model.Branch>> {
             let url = `${this.url}/projects/${projectId}/branches?account-key=${this.accountKey}&login=${this.login}`;
             return axios.post(url, request);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param branchId branch identifier
+         */
         getBranch(projectId: number, branchId: number): Promise<ResponseObject<Model.Branch>> {
             let url = `${this.url}/projects/${projectId}/branches/${branchId}?account-key=${this.accountKey}&login=${this.login}`;
             return axios.get(url);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param branchId branch identifier
+         */
         //TODO should be discussed with back end
         deleteBranch(projectId: number, branchId: number): Promise<void> {
             let url = `${this.url}/projects/${projectId}/branches/${branchId}?account-key=${this.accountKey}&login=${this.login}`;
             return axios.delete(url);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param branchId branch identifier
+         * @param request request body
+         */
         updateBranch(projectId: number, branchId: number, request: PatchRequest[]): Promise<ResponseObject<Model.Branch>> {
             let url = `${this.url}/projects/${projectId}/branches/${branchId}?account-key=${this.accountKey}&login=${this.login}`;
             return axios.patch(url, request);
--- src/directories/index.ts
+++ src/directories/index.ts
@@ -7,6 +7,13 @@ export namespace Directories {
 
     export class Api extends CrowdinApi {
 
+        /**
+         * @param projectId project identifier
+         * @param branchId filter directories by branchId
+         * @param directoryId filter directories by directoryId
+         * @param limit maximum number of items to retrieve (default 25)
+         * @param offset starting offset in the collection (default 0)
+         */
         listProjectDirectories(projectId: number, branchId?: number, directoryId?: number, limit?: number, offset?: number): Promise<ResponseList<Model.Directory>> {
             let url = `${this.url}/projects/${projectId}/directories?account-key=${this.accountKey}&login=${this.login}`;
             url = this.addQueryParam(url, 'branchId', branchId);
@@ -16,22 +23,39 @@ export namespace Directories {
             return axios.get(url);
         }
 
+        /**
+         * @param projectId project identifier 
+         * @param request request body
+         */
         createDirectory(projectId: number, request: Model.CreateDirectoryRequest): Promise<ResponseObject<Model.Directory>> {
             let url = `${this.url}/projects/${projectId}/directories?account-key=${this.accountKey}&login=${this.login}`;
             return axios.post(url, request);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param directoryId directory identifier
+         */
         getDirectory(projectId: number, directoryId: number): Promise<ResponseObject<Model.Directory>> {
             let url = `${this.url}/projects/${projectId}/directories/${directoryId}?account-key=${this.accountKey}&login=${this.login}`;
             return axios.get(url);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param directoryId directory identifier
+         */
         //TODO should be discussed with back end
         deleteDirectory(projectId: number, directoryId: number): Promise<void> {
             let url = `${this.url}/projects/${projectId}/directories/${directoryId}?account-key=${this.accountKey}&login=${this.login}`;
             return axios.delete(url);
         }
 
+        /**
+         * @param projectId project identifier
+         * @param directoryId directory identifier
+         * @param request request body
+         */
         updateDirectory(projectId: number, directoryId: number, request: PatchRequest[]): Promise<ResponseObject<Model.Directory>> {
             let url = `${this.url}/projects/${projectId}/directories/${directoryId}?account-key=${this.accountKey}&login=${this.login}`;
             return axios.patch(url, request);
