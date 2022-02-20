170b6fdece4999759f8a36e1c0d69a95d3ba7b78
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-02-20 12:58:06 +0000

    test: add tests for new methods

--- tests/workflows/api.test.ts
+++ tests/workflows/api.test.ts
@@ -9,11 +9,40 @@ describe('Workflows API', () => {
     };
     const api: Workflows = new Workflows(credentials);
     const id = 2;
+    const projectId = 4;
 
     const limit = 25;
 
     beforeAll(() => {
         scope = nock(api.url)
+            .get(`/projects/${projectId}/workflow-steps`, undefined, {
+                reqheaders: {
+                    Authorization: `Bearer ${api.token}`,
+                },
+            })
+            .reply(200, {
+                data: [
+                    {
+                        data: {
+                            id: id,
+                        },
+                    },
+                ],
+                pagination: {
+                    offset: 0,
+                    limit: limit,
+                },
+            })
+            .get(`/projects/${projectId}/workflow-steps/${id}`, undefined, {
+                reqheaders: {
+                    Authorization: `Bearer ${api.token}`,
+                },
+            })
+            .reply(200, {
+                data: {
+                    id: id,
+                },
+            })
             .get('/workflow-templates', undefined, {
                 reqheaders: {
                     Authorization: `Bearer ${api.token}`,
@@ -46,6 +75,18 @@ describe('Workflows API', () => {
 
     afterAll(() => {
         scope.done();
+    });
+
+    it('List Workflow steps', async () => {
+        const workflowSteps = await api.listWorkflowSteps(projectId);
+        expect(workflowSteps.data.length).toBe(1);
+        expect(workflowSteps.data[0].data.id).toBe(id);
+        expect(workflowSteps.pagination.limit).toBe(limit);
+    });
+
+    it('Get Workflow step info', async () => {
+        const workflowStep = await api.getWorkflowStep(projectId, id);
+        expect(workflowStep.data.id).toBe(id);
     });
 
     it('List Workflow Templates', async () => {
