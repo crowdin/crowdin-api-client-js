81f0dfd4af22c0d43f11821e8df35c5f06e61cb7
Yevheniy Oliynyk <evgen41293@gmail.com>
2023-05-27 14:51:52 +0300

    feat: organization webhooks api (#264)

---
+++ tests/organizationWebhooks/api.test.ts
@@ -0,0 +1,137 @@
+import * as nock from 'nock';
+import { Credentials, OrganizationWebhooks } from '../../src/index';
+
+describe('Organization Webhooks API', () => {
+    let scope: nock.Scope;
+    const credentials: Credentials = {
+        token: 'testToken',
+        organization: 'testOrg',
+    };
+    const api: OrganizationWebhooks = new OrganizationWebhooks(credentials);
+    const webhookId = 3;
+    const name = 'test';
+    const url = 'test.com';
+    const requestType = 'GET';
+
+    const limit = 25;
+
+    beforeAll(() => {
+        scope = nock(api.url)
+            .get('/webhooks', undefined, {
+                reqheaders: {
+                    Authorization: `Bearer ${api.token}`,
+                },
+            })
+            .reply(200, {
+                data: [
+                    {
+                        data: {
+                            id: webhookId,
+                        },
+                    },
+                ],
+                pagination: {
+                    offset: 0,
+                    limit: limit,
+                },
+            })
+            .post(
+                '/webhooks',
+                {
+                    name: name,
+                    url: url,
+                    events: [],
+                    requestType: requestType,
+                },
+                {
+                    reqheaders: {
+                        Authorization: `Bearer ${api.token}`,
+                    },
+                },
+            )
+            .reply(200, {
+                data: {
+                    id: webhookId,
+                },
+            })
+            .get(`/webhooks/${webhookId}`, undefined, {
+                reqheaders: {
+                    Authorization: `Bearer ${api.token}`,
+                },
+            })
+            .reply(200, {
+                data: {
+                    id: webhookId,
+                },
+            })
+            .delete(`/webhooks/${webhookId}`, undefined, {
+                reqheaders: {
+                    Authorization: `Bearer ${api.token}`,
+                },
+            })
+            .reply(200)
+            .patch(
+                `/webhooks/${webhookId}`,
+                [
+                    {
+                        value: name,
+                        op: 'replace',
+                        path: '/name',
+                    },
+                ],
+                {
+                    reqheaders: {
+                        Authorization: `Bearer ${api.token}`,
+                    },
+                },
+            )
+            .reply(200, {
+                data: {
+                    id: webhookId,
+                    name: name,
+                },
+            });
+    });
+
+    afterAll(() => {
+        scope.done();
+    });
+
+    it('List webhooks', async () => {
+        const webhooks = await api.listWebhooks();
+        expect(webhooks.data.length).toBe(1);
+        expect(webhooks.data[0].data.id).toBe(webhookId);
+        expect(webhooks.pagination.limit).toBe(limit);
+    });
+
+    it('Add webhook', async () => {
+        const webhook = await api.addWebhook({
+            name: name,
+            url: url,
+            events: [],
+            requestType: requestType,
+        });
+        expect(webhook.data.id).toBe(webhookId);
+    });
+
+    it('Get webhook', async () => {
+        const webhook = await api.getWebhook(webhookId);
+        expect(webhook.data.id).toBe(webhookId);
+    });
+
+    it('Delete webhook', async () => {
+        await api.deleteWebhook(webhookId);
+    });
+
+    it('Edit webhook', async () => {
+        const webhook = await api.editWebhook(webhookId, [
+            {
+                op: 'replace',
+                path: '/name',
+                value: name,
+            },
+        ]);
+        expect(webhook.data.id).toBe(webhookId);
+        expect(webhook.data.name).toBe(name);
+    });
+});
