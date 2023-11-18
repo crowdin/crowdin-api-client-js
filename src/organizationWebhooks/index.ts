318248f3822182792d0fd634daed66a872cc86ea
Yevheniy Oliynyk <evgen41293@gmail.com>
2023-11-18 11:51:18 +0200

    org webhooks

--- src/organizationWebhooks/index.ts
+++ src/organizationWebhooks/index.ts
@@ -1,4 +1,5 @@
 import { CrowdinApi, PaginationOptions, PatchRequest, ResponseList, ResponseObject } from '../core';
+import { WebhooksModel } from '../webhooks';
 
 /**
  * Webhooks allow you to collect information about events that happen in your Crowdin account.
@@ -10,7 +11,7 @@ export class OrganizationWebhooks extends CrowdinApi {
      * @param options optional pagination parameters for the request
      * @see https://developer.crowdin.com/api/v2/#operation/api.webhooks.getMany
      */
-    listWebhooks(options?: PaginationOptions): Promise<ResponseList<OrganizationWebhooksModel.Webhook>> {
+    listWebhooks(options?: PaginationOptions): Promise<ResponseList<WebhooksModel.Webhook>> {
         const url = `${this.url}/webhooks`;
         return this.getList(url, options?.limit, options?.offset);
     }
@@ -19,9 +20,7 @@ export class OrganizationWebhooks extends CrowdinApi {
      * @param request request body
      * @see https://developer.crowdin.com/api/v2/#operation/api.webhooks.post
      */
-    addWebhook(
-        request: OrganizationWebhooksModel.AddWebhookRequest,
-    ): Promise<ResponseObject<OrganizationWebhooksModel.Webhook>> {
+    addWebhook(request: WebhooksModel.AddWebhookRequest): Promise<ResponseObject<WebhooksModel.Webhook>> {
         const url = `${this.url}/webhooks`;
         return this.post(url, request, this.defaultConfig());
     }
@@ -30,7 +29,7 @@ export class OrganizationWebhooks extends CrowdinApi {
      * @param webhookId webhook identifier
      * @see https://developer.crowdin.com/api/v2/#operation/api.webhooks.get
      */
-    getWebhook(webhookId: number): Promise<ResponseObject<OrganizationWebhooksModel.Webhook>> {
+    getWebhook(webhookId: number): Promise<ResponseObject<WebhooksModel.Webhook>> {
         const url = `${this.url}/webhooks/${webhookId}`;
         return this.get(url, this.defaultConfig());
     }
@@ -49,46 +48,8 @@ export class OrganizationWebhooks extends CrowdinApi {
      * @param request request body
      * @see https://developer.crowdin.com/api/v2/#operation/api.webhooks.patch
      */
-    editWebhook(
-        webhookId: number,
-        request: PatchRequest[],
-    ): Promise<ResponseObject<OrganizationWebhooksModel.Webhook>> {
+    editWebhook(webhookId: number, request: PatchRequest[]): Promise<ResponseObject<WebhooksModel.Webhook>> {
         const url = `${this.url}/webhooks/${webhookId}`;
         return this.patch(url, request, this.defaultConfig());
     }
-}
-
-export namespace OrganizationWebhooksModel {
-    export interface Webhook {
-        id: number;
-        name: string;
-        url: string;
-        events: Event[];
-        headers: string[];
-        payload: string[];
-        isActive: boolean;
-        batchingEnabled: boolean;
-        requestType: RequestType;
-        contentType: ContentType;
-        createdAt: string;
-        updatedAt: string;
-    }
-
-    export interface AddWebhookRequest {
-        name: string;
-        url: string;
-        isActive?: boolean;
-        batchingEnabled?: boolean;
-        contentType?: ContentType;
-        events: Event[];
-        headers?: Record<string, string>;
-        requestType: RequestType;
-        payload?: any;
-    }
-
-    export type ContentType = 'multipart/form-data' | 'application/json' | 'application/x-www-form-urlencoded';
-
-    export type Event = 'project.created' | 'project.deleted' | 'group.created' | 'group.deleted';
-
-    export type RequestType = 'POST' | 'GET';
 }
