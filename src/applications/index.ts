3a3946fa80fcc8e385da72f37be586f4faf6a922
AksBad007 <arora.akshat007@gmail.com>
2023-12-31 05:55:36 +0530

    support for new endpoints

--- src/applications/index.ts
+++ src/applications/index.ts
@@ -1,4 +1,4 @@
-import { CrowdinApi, ResponseObject } from '../core';
+import { CrowdinApi, ResponseObject, PatchRequest, Pagination } from '../core';
 
 /**
  * Crowdin Apps are web applications that can be integrated with Crowdin to extend its functionality.
@@ -6,6 +6,66 @@ import { CrowdinApi, ResponseObject } from '../core';
  * Use the API to manage the necessary app data.
  */
 export class Applications extends CrowdinApi {
+    /**
+     * @param options optional pagination parameters for the request
+     * @see https://developer.crowdin.com/api/v2/#operation/api.applications.installations.getMany
+     */
+    listApplicationInstallations(options?: Pagination): Promise<ResponseObject<ApplicationsModel.ApplicationData>> {
+        const url = `${this.url}/applications/installations`;
+        return this.getList(url, options?.limit, options?.offset);
+    }
+
+    /**
+     * @param applicationUrl Manifest URL
+     * @param permissions application permissions
+     * @see https://developer.crowdin.com/api/v2/#operation/api.applications.installations.post
+     */
+    installApplication(
+        applicationUrl: string,
+        permissions?: ApplicationsModel.ApplicationPermissions,
+    ): Promise<ResponseObject<ApplicationsModel.ApplicationData>> {
+        const url = `${this.url}/applications/installations`;
+        return this.post(url, { applicationUrl, permissions }, this.defaultConfig());
+    }
+
+    /**
+     * @param applicationId application identifier
+     * @see https://developer.crowdin.com/api/v2/#operation/api.applications.installations.get
+     */
+    getApplicationInstallation(applicationId: string): Promise<ResponseObject<ApplicationsModel.ApplicationData>> {
+        const url = `${this.url}/applications/installations/${applicationId}`;
+        return this.get(url, this.defaultConfig());
+    }
+
+    /**
+     * @param applicationId application identifier
+     * @param force force delete the application
+     * @see https://developer.crowdin.com/api/v2/#operation/api.applications.installations.delete
+     */
+    deleteApplicationInstallation(
+        applicationId: string,
+        force?: boolean,
+    ): Promise<ResponseObject<ApplicationsModel.ApplicationData>> {
+        const url = `${this.url}/applications/installations/${applicationId}`;
+        if (force) {
+            this.addQueryParam(url, 'force', String(force));
+        }
+        return this.delete(url, this.defaultConfig());
+    }
+
+    /**
+     * @param applicationId application identifier
+     * @param request request body
+     * @see https://developer.crowdin.com/api/v2/#operation/api.applications.installations.patch
+     */
+    editApplicationInstallation(
+        applicationId: string,
+        request: PatchRequest[],
+    ): Promise<ResponseObject<ApplicationsModel.ApplicationData>> {
+        const url = `${this.url}/applications/installations/${applicationId}`;
+        return this.patch(url, request, this.defaultConfig());
+    }
+
     /**
      * @param applicationId application identifier
      * @param path path implemented by the application
@@ -78,5 +138,16 @@ export class Applications extends CrowdinApi {
 export namespace ApplicationsModel {
     export interface ApplicationData {
         [key: string]: any;
+    }
+
+    export interface ApplicationPermissions {
+        user: {
+            value: 'all' | 'owner' | 'managers' | 'guests' | 'restricted';
+            ids: Array<number>;
+        };
+        project: {
+            value: 'own' | 'restricted';
+            ids: Array<number>;
+        };
     }
 }
